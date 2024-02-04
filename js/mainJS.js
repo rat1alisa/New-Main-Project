b3bd214fb96fa0ba94813cfd35dfbdf2921959e4
Alice <alekseevaalisa763@gmail.com>
2024-02-04 19:40:51 +0300

    04.02.24 commit

---
+++ js/mainJS.js
@@ -0,0 +1,21 @@
+
+
+/*let findButton = document.querySelector(".menuElement");
+let menu = document.getElementById("popMenu");
+
+if(findButton){
+
+    findButton.addEventListener("click", () =>{
+        if(findButton.classList.contains("pushed")) {
+            findButton.classList.remove("pushed");
+            menu.classList.remove("opened");
+        }
+        else{
+            findButton.classList.add("pushed");
+            menu.classList.add("opened");
+        }
+    })
+}*/
+
+
+
