7ab9fe0ca569de1b62cd3f8a5ce1a5aea4e6e68a
Alice <alekseevaalisa763@gmail.com>
2024-02-11 16:11:12 +0300

    4 commit

--- js/mainJS.js
+++ js/mainJS.js
@@ -18,4 +18,12 @@ if(findButton){
 }*/
 
 
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,//сколько будет отображаться
+        slidesToScroll: 3
+      });
+});
+
 
