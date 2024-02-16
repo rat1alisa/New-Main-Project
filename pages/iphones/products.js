f7d2bc7ad2bc4b99fdb9f20e924783fcb48f0b35
Alice <alekseevaalisa763@gmail.com>
2024-02-16 19:36:02 +0300

    commit big

---
+++ pages/iphones/products.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 15 Pro Max",
+        price: "From $1199",
+        images: [
+            "https://mobilworld.by/upload/iblock/9c5/9c566a95b3fd15454e27cd7a21c8ca02.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15promax-digitalmat-gallery-1-202309_GEO_US?wid=364&hei=333&fmt=png-alpha&.v=1693346851426",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15promax-digitalmat-gallery-2-202309?wid=364&hei=333&fmt=png-alpha&.v=1693081545196",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15promax-digitalmat-gallery-4-202309?wid=364&hei=333&fmt=png-alpha&.v=1693081545196"
+        ]
+    },
+    {
+        id: 2,
+        name: "iPhone 15 Pro",
+        price: "From $999",
+        images: [
+            "https://my-store.by/wp-content/uploads/2023/09/Group-80.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15pro-digitalmat-gallery-1-202309_GEO_US?wid=364&hei=333&fmt=png-alpha&.v=1693346851451",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15pro-digitalmat-gallery-2-202309?wid=364&hei=333&fmt=png-alpha&.v=1693081541434",
+            "https://mobilworld.by/upload/iblock/c7e/c7efdccd3f82b1c92d84181ddede5572.png"
+        ]
+    },
+    {
+        id: 3,
+        name: "iPhone 15",
+        price: "From $899",
+        images: [
+            "https://mobilworld.by/upload/iblock/bda/bda42dff2feebdcadb7dd7ee5eac6ddd.png",
+            "https://d2xamzlzrdbdbn.cloudfront.net/products/c635b161-cfb9-4fcf-8e7f-c2ba5114a1e522090602.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15-digitalmat-gallery-2-202309?wid=364&hei=333&fmt=png-alpha&.v=1693011169058",
+            "https://mobilecare.sk/wp-content/uploads/2023/09/apple-iphone-15-plus-512-gb-modra-mu1p3sx-a-466631.jpg"
+        ]
+    },
+    {
+        id: 4,
+        name: "iPhone 14 Plus",
+        price: "From $799",
+        images: [
+            "https://manyphone.ru/upload/iblock/aa3/zn6myzavb1av890ryplr4zhw6mao6ccm.jpg",
+            "https://static9.edstatic.net/product_images/1280x1280/resize/1_dn59q9yv.png?v=1",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone14plus-digitalmat-gallery-2-202209?wid=364&hei=333&fmt=png-alpha&.v=1662055813701",
+            "https://media.studio7thailand.com/93310/iPhone_14_Plus_1-square_medium.jpg"
+        ]
+    },
+    {
+        id: 5,
+        name: "iPhone 13",
+        price: "From $599",
+        images: [
+            "https://mobillife.by/images/virtuemart/product/30059037b.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone13-digitalmat-gallery-1-202203_GEO_US?wid=364&hei=333&fmt=png-alpha&.v=1677662648569",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone13-digitalmat-gallery-2-202203?wid=364&hei=333&fmt=png-alpha&.v=1644988764167",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone13-digitalmat-gallery-3-202203?wid=364&hei=333&fmt=png-alpha&.v=1644988765122"
+        ]
+    },
+    {
+        id: 6,
+        name: "iPhone SE",
+        price: "From $429",
+        images: [
+            "https://app-minsk.by/files/items/4440/icon_480x390.png",
+            "https://static9.edstatic.net/product_images/1280x1280/resize/1_dn59q9yv.png?v=1",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone14plus-digitalmat-gallery-2-202209?wid=364&hei=333&fmt=png-alpha&.v=1662055813701",
+            "https://media.studio7thailand.com/93310/iPhone_14_Plus_1-square_medium.jpg"
+        ]
+    }
+];
