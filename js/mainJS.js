082d48ab725676e5816677f67520760613ee0bfc
Alice <alekseevaalisa763@gmail.com>
2024-02-23 19:37:25 +0300

    commit blur

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,10 +1,24 @@
 
 
-$(document).ready(function(){
-    $('.slider').slick({
-        infinite: true,
-        slidesToShow: 3,//сколько будет отображаться
-        slidesToScroll: 3
-      });
+
+$('.autoSlider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
 });
+
+//#h - id наведения
+
+
+document.getElementById("ulLiBlur").addEventListener("mouseenter", () => {
+    document.getElementById("main1").style.filter = "blur(2px)";
+});
+document.getElementById("ulLiBlur").addEventListener("mouseleave", () => {
+  document.getElementById("main1").style.filter = "none";
+});
+
+
+
+// ИЩЕМ ПО КЛАССУ И ПРОХОДИМ ПО НИМ ВСЕМ И КАЖДОМУ ДОБАВЛЯЕМ ЭТИ СТИЛИ
 
