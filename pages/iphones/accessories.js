de41582b8bfbeeb0f3f6263e409757dabace2b3e
Alice <alekseevaalisa763@gmail.com>
2024-03-17 19:39:02 +0300

    17/03

---
+++ pages/iphones/accessories.js
@@ -0,0 +1,27 @@
+const accessories = [
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT4J3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693594197616",
+        name: "iPhone 15 Pro Max FineWoven Case",
+        price: "49"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT223_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693248281480",
+        name: "iPhone 15 Pro Clear Case",
+        price: "39"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT243?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1692994356181",
+        name: "iPhone FineWoven Wallet",
+        price: "59"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHXH3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1661269793559",
+        name: "MagSafe Charger",
+        price: "29"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MU7T2_AV1_GEO_US?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1542406390837",
+        name: "USB-C Power Adapter",
+        price: "19"
+    },
+]
