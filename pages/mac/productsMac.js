622cb43c7a2df20ec002bc10611a479cb2f95f90
Alice <alekseevaalisa763@gmail.com>
2024-03-22 19:00:57 +0300

    22/03/24

---
+++ pages/mac/productsMac.js
@@ -0,0 +1,41 @@
+
+
+const products = [
+    {
+        id: 1,
+        name: "Mac",
+        price: "From $1299",
+        images: [
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-imac-24-202310?wid=1200&hei=1000&fmt=jpeg&qlt=90&.v=1697229623322",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-1-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004117",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-3-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004001",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-4-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559005165"
+        ],
+        parametrs: [
+            {
+                icon: "gg-shape-square",
+                value: "A titanium bezel"
+            },
+            {
+                icon: "gg-arrow-long-right-r",
+                value: "USB-C port"
+            },
+            {
+                icon: "gg-camera",
+                value: "New 48-megapixel camera"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "The new A17 Bionic processor"
+            }
+        ],
+        colors: [
+            "#a7a6a3",
+            "#cfcfcd",
+            "#415C71",
+            "#59595c"
+        ],
+        
+    }
+    
+];
