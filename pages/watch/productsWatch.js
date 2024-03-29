8382994cbf92d3b0e5c728080453e428dd3bbd62
Alice <alekseevaalisa763@gmail.com>
2024-03-29 19:27:50 +0300

    29/03/24

---
+++ pages/watch/productsWatch.js
@@ -0,0 +1,126 @@
+
+
+const products = [
+    {
+        id: 1,
+        name: "Apple Watch series 9",
+        price: "From $399",
+        images: [
+            "https://my-store.by/wp-content/uploads/2023/09/Group-130.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-s9-digitalmat-gallery-2-202309?wid=728&hei=666&fmt=png-alpha&.v=1692847849852",
+            "https://redstore.by/wp-content/uploads/2023/09/watch-9-steel-41-3.png",
+
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "Supercharged by the Apple M3 chip, so everything you do on your iMac is incredibly fast and responsive"
+            },
+            {
+                icon: "gg-camera",
+                value: "Look sharp and sound great with the 1080p FaceTime HD camera"
+            },
+            {
+                icon: "gg-arrow-long-right-r",
+                value: "Immersive 24-inch 4.5K Retina display Footnote¹"
+            },
+        ],
+        colors: [
+            "#FF5733",
+            "#FFCE06",
+            "#008967",
+            "#014E74 ",
+            "#2500B0"
+        ],
+        
+    },
+    {
+        id: 2,
+        name: "Apple Watch Ultra 2",
+        price: "From $699",
+        images: [
+            "https://inventstore.in/wp-content/uploads/2023/09/watch-ultra-double-1.webp",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-ultra2-digitalmat-gallery-1-202309?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1692603382757",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-ultra2-digitalmat-gallery-3-202309?wid=728&hei=666&fmt=png-alpha&.v=1692603384550",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-ultra2-digitalmat-gallery-5-202309?wid=728&hei=666&fmt=png-alpha&.v=1692603386501"
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "With M3, M3 Pro, or M3 Max, our most advanced chips for personal computers"
+            },
+            {
+                icon: "gg-battery-empty",
+                value: "Go all day with up to 22 hours of battery life Footnote¹"
+            },
+            {
+                icon: "gg-camera",
+                value: "Look sharp and sound clear — anywhere — with the 1080p FaceTime HD camera"
+            },
+        ],
+        colors: [
+            "#979797",
+        ],
+        
+    },
+    {
+        id: 3,
+        name: "Apple Watch SE",
+        price: "From $249",
+        images: [
+            "https://redstore.by/wp-content/uploads/2022/09/Apple-Watch-se-2022-40-midnight-0.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-1-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004117",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-3-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004001",
+            "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:540/bg:f6f6f6/q:100/plain/s3://catalog-products/231205074649919687/231205080743983558.png@webp"
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "Get more done faster — The powerful 8-core CPU and up to 10-core GPU of the Apple M1 chip keep things running smoothly"
+            },
+            {
+                icon: "gg-display-flex",
+                value: "A brilliant display — The 13.6-inch Liquid Retina display supports 1 billion colors"
+            },
+            {
+                icon: "gg-edit-highlight",
+                value: "Portable design — Lightweight and under half an inch thin, so you can take MacBook Air anywhere you go"
+            }
+        ],
+        colors: [
+            "#a7a6a3",
+            "#cfcfcd",
+            "#415C71",
+            "#59595c"
+        ],
+        
+    },
+    {
+        id: 4,
+        name: "Apple Watch Hermès",
+        price: "From $599",
+        images: [
+            "https://main-cdn.sbermegamarket.ru/big1/hlr-system/-10/646/354/381/121/11/100047510721b0.jpeg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-digitalmat-gallery-3-202301?wid=728&hei=666&fmt=png-alpha&.v=1670038165453",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-digitalmat-gallery-5-202301_GEO_US?wid=728&hei=666&fmt=png-alpha&.v=1670916363771",
+        ],
+        parametrs: [
+            {
+                icon: "gg-pen",
+                value: "Customize your setup using Mac mini’s two or four Thunderbolt 4 ports, two USB-A ports, HDMI, and Gigabit Ethernet"
+            },
+            {
+                icon: "gg-arrows-exchange-alt",
+                value: "Complete your system with Studio Display and Magic accessories"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "Do more and do it faster with the next-generation M2 or M2 Pro chip"
+            }
+        ],
+        colors: [
+            "#8C8C8C",
+        ],
+        
+    },
+];
