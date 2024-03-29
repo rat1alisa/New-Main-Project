de2d6b9bc35c115bbc845e4aae3e998c9f91c150
Alice <alekseevaalisa763@gmail.com>
2024-03-29 19:27:16 +0300

    29/03/24

---
+++ pages/ipad/productsIPad.js
@@ -0,0 +1,187 @@
+
+
+const products = [
+    {
+        id: 1,
+        name: "iPad Air",
+        price: "From $599",
+        images: [
+            "https://djshop.by/images/detailed/80/10.9-inch_iPad_Air_Green_j9ok-qd.webp",
+            "https://media.ldlc.com/r1600/ld/products/00/05/82/50/LD0005825024_1.jpg",
+            "https://tablets.com.co/wp-content/uploads/2022/04/Apple-10.9-inch-iPad-Air-Wi-Fi-64GB-Space-Grey-Foto-III-tablets.com_.co_-scaled.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipadair-digitalmat-gallery-1-202203?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1644962768135",
+
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "Supercharged by the Apple M3 chip, so everything you do on your iMac is incredibly fast and responsive"
+            },
+            {
+                icon: "gg-camera",
+                value: "Look sharp and sound great with the 1080p FaceTime HD camera"
+            },
+            {
+                icon: "gg-arrow-long-right-r",
+                value: "Immersive 24-inch 4.5K Retina display Footnote¹"
+            },
+        ],
+        colors: [
+            "#CFCFCF",
+            "#787878",
+            "#EDCFD6",
+            "#CFEDD7",
+            "#CFDFED"
+        ],
+        
+    },
+    {
+        id: 2,
+        name: "iPad Pro 2021",
+        price: "From $799",
+        images: [
+            "https://redstore.by/wp-content/uploads/2023/01/Apple-iPad-Pro-12-9-M2-2022-Silver.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipadpro11-digitalmat-gallery-4-202210?wid=728&hei=666&fmt=png-alpha&.v=1664477161238",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipadpro11-digitalmat-gallery-5-202210?wid=728&hei=666&fmt=png-alpha&.v=1664477161177",
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "With M3, M3 Pro, or M3 Max, our most advanced chips for personal computers"
+            },
+            {
+                icon: "gg-battery-empty",
+                value: "Go all day with up to 22 hours of battery life Footnote¹"
+            },
+            {
+                icon: "gg-camera",
+                value: "Look sharp and sound clear — anywhere — with the 1080p FaceTime HD camera"
+            },
+        ],
+        colors: [
+            "#D3D3D3",
+            "#979797",
+        ],
+        
+    },
+    {
+        id: 3,
+        name: "iPad mini",
+        price: "From $999",
+        images: [
+            "https://futureit.om/media/catalog/product/cache/1b86af179eec0db7d828498c04f28830/u/n/untitled_500_500_px_-_2023-01-05t101205.237.jpg",
+            "https://powermaccenter.com/cdn/shop/files/iPad_mini_6th_generation_Wi-Fi_Pink_PDP_Image_Position-2__en-US_1445x.jpg?v=1689783804",
+            "https://www.rstore.it/media/catalog/product/cache/bb5991ed8856d22ce5c275e41c5f78f9/A/_/A_iPadmini_WC_2021_1.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-mini-digitalmat-gallery-5-202111?wid=730&hei=666&fmt=png-alpha&.v=1635183176000"
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "Get more done faster — The powerful 8-core CPU and up to 10-core GPU of the Apple M1 chip keep things running smoothly"
+            },
+            {
+                icon: "gg-display-flex",
+                value: "A brilliant display — The 13.6-inch Liquid Retina display supports 1 billion colors"
+            },
+            {
+                icon: "gg-edit-highlight",
+                value: "Portable design — Lightweight and under half an inch thin, so you can take MacBook Air anywhere you go"
+            }
+        ],
+        colors: [
+            "#a7a6a3",
+            "#EDCFD4",
+            "#CFD0ED",
+            "#E3E4D4 "
+        ],
+        
+    },
+    {
+        id: 4,
+        name: "iPad (10th gen.)",
+        price: "From $499",
+        images: [
+            "https://cdn.talaco.net/talaco/assets/images/2022/10/31163707/ipad-2022-hero-pink-wifi-select.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-digitalmat-gallery-3-202210?wid=728&hei=666&fmt=png-alpha&.v=1664392871324",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-digitalmat-gallery-5-202210?wid=728&hei=666&fmt=png-alpha&.v=1664392871595",
+        ],
+        parametrs: [
+            {
+                icon: "gg-pen",
+                value: "Customize your setup using Mac mini’s two or four Thunderbolt 4 ports, two USB-A ports, HDMI, and Gigabit Ethernet"
+            },
+            {
+                icon: "gg-arrows-exchange-alt",
+                value: "Complete your system with Studio Display and Magic accessories"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "Do more and do it faster with the next-generation M2 or M2 Pro chip"
+            }
+        ],
+        colors: [
+            "#E8E8E8",
+            "#EAD81F",
+            "#E94E74",
+            "#73ADC8",
+        ],
+        
+    },
+    {
+        id: 5,
+        name: "Studio Display",
+        price: "From $1599",
+        images: [
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-studio-display-202203?wid=400&hei=500&fmt=p-jpg&qlt=95&.v=1645558306366",
+            "https://content2.onliner.by/catalog/device/large/16486047c4ca418786763a242aeac585.jpeg",
+            "https://m.media-amazon.com/images/I/51afAQocEQL._AC_UF894,1000_QL80_.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/studio-display-digitalmat-gallery-3-202203?wid=728&hei=666&fmt=png-alpha&.v=1645207691982"
+        ],
+        parametrs: [
+            {
+                icon: "gg-arrows-expand-right",
+                value: "27-inch 5K Retina display draws you in with 14.7 million pixels and one billion colors"
+            },
+            {
+                icon: "gg-controller",
+                value: "12MP Ultra Wide camera with Center Stage — powered by Apple silicon — allows for more engaging video calls"
+            },
+            {
+                icon: "gg-arrows-exchange-alt",
+                value: "Pairs perfectly with any Mac, including the all-new Mac Studio. Add new matching Magic accessories to complete your studio."
+            },
+        ],
+        colors: [
+            "#8C8C8C",
+        ],
+        
+    },
+    {
+        id: 6,
+        name: "Mac Pro",
+        price: "From $1299",
+        images: [
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-pro-digitalmat-gallery-1-202306?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1683842315753",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-pro-digitalmat-gallery-2-202306?wid=728&hei=666&fmt=png-alpha&.v=1683842315746",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-pro-digitalmat-gallery-5-202306?wid=728&hei=666&fmt=png-alpha&.v=1683842315754",
+        ],
+        parametrs: [
+            {
+                icon: "gg-smartphone-chip",
+                value: "Supercharged by M2 Ultra, our most powerful and capable chip"
+            },
+            {
+                icon: "gg-shape-square",
+                value: "Runs cool and quiet, even under intensive workloads"
+            },
+            {
+                icon: "gg-arrow-long-right-r",
+                value: "Comes with Magic accessories. Complete your setup with Pro Display XDR."
+            },
+        ],
+        colors: [
+            "#8C8C8C",
+        ],
+        
+    },
+];
