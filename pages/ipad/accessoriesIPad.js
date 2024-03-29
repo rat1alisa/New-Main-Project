01cc4b02097ad7127ab1a49b061fc64ac14c482d
Alice <alekseevaalisa763@gmail.com>
2024-03-29 19:27:05 +0300

    29/03/24

---
+++ pages/ipad/accessoriesIPad.js
@@ -0,0 +1,28 @@
+
+const accessories = [
+    {
+        photo: "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/bg:fff/plain/s3://catalog-products/181031080443550129/181115150015021492.jpg@webp",
+        name: "Smart Folio for iPad",
+        price: "79"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MUWA3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1695933856697",
+        name: "Apple Pencil (USB-C)",
+        price: "79"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQDP3_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1664481445994",
+        name: "Magic Keyboard Folio for iPad",
+        price: "249"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MNA73?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1645642309453",
+        name: "Smart Folio for iPad Air",
+        price: "79"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MU8F2?wid=400&hei=400&fmt=jpeg&qlt=90&.v=1540596407165",
+        name: "Apple Pencil",
+        price: "129"
+    },
+]
