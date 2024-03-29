dc200e0494d4096839b43138866f642da17b19ab
Alice <alekseevaalisa763@gmail.com>
2024-03-29 19:27:30 +0300

    29/03/24

---
+++ pages/mac/accessoriesMac.js
@@ -0,0 +1,28 @@
+
+const accessories = [
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MK293?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1628006485000",
+        name: "Magic Keyboard with Touch ID",
+        price: "179"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MNWM3?wid=400&hei=400&fmt=jpeg&qlt=90&.v=1652736144126",
+        name: "USB-C Port Compact Power Adapter",
+        price: "59"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MPL33?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1698168622274",
+        name: "USB-C to MagSafe 3 Cable",
+        price: "49"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MK2E3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1626468075000",
+        name: "Magic Mouse",
+        price: "79"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MK2D3_AV3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1626390717000",
+        name: "Magic Trackpad",
+        price: "129"
+    },
+]
