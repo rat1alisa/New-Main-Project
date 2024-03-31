139c1c7a6e8b8f86cbcb7a0059021a7874a611d4
Alice <alekseevaalisa763@gmail.com>
2024-03-31 11:55:54 +0300

    31/03/24

--- pages/watch/accessoriesWatch.js
+++ pages/watch/accessoriesWatch.js
@@ -1,28 +1,28 @@
 
 const accessories = [
     {
-        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MK293?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1628006485000",
-        name: "Magic Keyboard with Touch ID",
-        price: "179"
-    },
-    {
-        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MNWM3?wid=400&hei=400&fmt=jpeg&qlt=90&.v=1652736144126",
-        name: "USB-C Port Compact Power Adapter",
-        price: "59"
-    },
-    {
-        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MPL33?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1698168622274",
-        name: "USB-C to MagSafe 3 Cable",
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MW4Q3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1707853692193",
+        name: "Soft Mint Sport Loop",
         price: "49"
     },
     {
-        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MK2E3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1626468075000",
-        name: "Magic Mouse",
-        price: "79"
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWMY3ref?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1707791911296",
+        name: "Soft Mint Sport Band",
+        price: "49"
     },
     {
-        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MK2D3_AV3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1626390717000",
-        name: "Magic Trackpad",
-        price: "129"
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MTJ93ref?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1692936515148",
+        name: "Pacific Blue Magnetic Link",
+        price: "99"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MTL63?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693086527187",
+        name: "Sequoia/Orange Nike Sport Loop",
+        price: "49"
+    },
+    {
+        photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MTJK3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693087386445",
+        name: "Apple Watch Hermès",
+        price: "349"
     },
 ]
