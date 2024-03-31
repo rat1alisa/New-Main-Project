7cafe85187bd9e8b753d167388966cb6b6725478
Alice <alekseevaalisa763@gmail.com>
2024-03-31 11:56:00 +0300

    31/03/24

--- pages/watch/productsWatch.js
+++ pages/watch/productsWatch.js
@@ -3,7 +3,7 @@
 const products = [
     {
         id: 1,
-        name: "Apple Watch series 9",
+        name: "Apple Watch Series 9",
         price: "From $399",
         images: [
             "https://my-store.by/wp-content/uploads/2023/09/Group-130.png",
@@ -13,24 +13,23 @@ const products = [
         ],
         parametrs: [
             {
-                icon: "gg-smartphone-chip",
-                value: "Supercharged by the Apple M3 chip, so everything you do on your iMac is incredibly fast and responsive"
+                icon: "gg-shape-square",
+                value: "Brighter Always-On Retina display with a durable, crack-resistant front crystal"
             },
             {
-                icon: "gg-camera",
-                value: "Look sharp and sound great with the 1080p FaceTime HD camera"
+                icon: "gg-card-hearts",
+                value: "Get deep insights into your physical and mental health including heart rate, Footnote 1 mood, and more"
             },
             {
-                icon: "gg-arrow-long-right-r",
-                value: "Immersive 24-inch 4.5K Retina display Footnote¹"
+                icon: "gg-drive",
+                value: "The all-new S9 SiP powers a magical new way to use your watch without touching the screen"
             },
         ],
         colors: [
-            "#FF5733",
-            "#FFCE06",
-            "#008967",
-            "#014E74 ",
-            "#2500B0"
+            "#EFD1DF",
+            "#E5E5E5",
+            "#BFBFBF",
+            "#404040",
         ],
         
     },
@@ -46,16 +45,16 @@ const products = [
         ],
         parametrs: [
             {
-                icon: "gg-smartphone-chip",
-                value: "With M3, M3 Pro, or M3 Max, our most advanced chips for personal computers"
+                icon: "gg-circleci",
+                value: "The most accurate GPS in a sports watch in dense urban environments Footnote 1"
             },
             {
                 icon: "gg-battery-empty",
-                value: "Go all day with up to 22 hours of battery life Footnote¹"
+                value: "Up to 36 hours of battery life, up to 72 hours on low power settings, Footnote 2 and up to 17 hours of workout use in Low Power Mode Footnote 3"
             },
             {
-                icon: "gg-camera",
-                value: "Look sharp and sound clear — anywhere — with the 1080p FaceTime HD camera"
+                icon: "gg-drive",
+                value: "The all-new S9 SiP powers a magical new way to use your watch without touching the screen"
             },
         ],
         colors: [
@@ -69,22 +68,21 @@ const products = [
         price: "From $249",
         images: [
             "https://redstore.by/wp-content/uploads/2022/09/Apple-Watch-se-2022-40-midnight-0.png",
-            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-1-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004117",
-            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac24-digitalmat-gallery-3-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004001",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-se-digitalmat-gallery-3-202403?wid=728&hei=666&fmt=png-alpha&.v=1708659608399",
             "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:540/bg:f6f6f6/q:100/plain/s3://catalog-products/231205074649919687/231205080743983558.png@webp"
         ],
         parametrs: [
             {
-                icon: "gg-smartphone-chip",
-                value: "Get more done faster — The powerful 8-core CPU and up to 10-core GPU of the Apple M1 chip keep things running smoothly"
+                icon: "gg-components",
+                value: "Call and message even when you don't have your iPhone nearby with GPS + Cellular models Footnote 1"
             },
             {
-                icon: "gg-display-flex",
-                value: "A brilliant display — The 13.6-inch Liquid Retina display supports 1 billion colors"
+                icon: "gg-code",
+                value: "Innovative safety features including Emergency SOS,  footnote  2 Fall Detection, and Crash Detection"
             },
             {
-                icon: "gg-edit-highlight",
-                value: "Portable design — Lightweight and under half an inch thin, so you can take MacBook Air anywhere you go"
+                icon: "",
+                value: "Get advanced workout metrics and views"
             }
         ],
         colors: [
@@ -101,22 +99,23 @@ const products = [
         price: "From $599",
         images: [
             "https://main-cdn.sbermegamarket.ru/big1/hlr-system/-10/646/354/381/121/11/100047510721b0.jpeg",
-            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-digitalmat-gallery-3-202301?wid=728&hei=666&fmt=png-alpha&.v=1670038165453",
-            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-digitalmat-gallery-5-202301_GEO_US?wid=728&hei=666&fmt=png-alpha&.v=1670916363771",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-hermes-digitalmat-gallery-2-202403?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1708249275498",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-hermes-digitalmat-gallery-5-202403?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1708671804497",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-hermes-digitalmat-gallery-3-202403?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1708249275582",
         ],
         parametrs: [
             {
-                icon: "gg-pen",
-                value: "Customize your setup using Mac mini’s two or four Thunderbolt 4 ports, two USB-A ports, HDMI, and Gigabit Ethernet"
+                icon: "gg-asterisk",
+                value: "Exclusive Hermès bands and watch faces"
             },
             {
-                icon: "gg-arrows-exchange-alt",
-                value: "Complete your system with Studio Display and Magic accessories"
+                icon: "gg-drive",
+                value: "The all-new S9 SiP powers a magical new way to use your watch without touching the screen"
             },
             {
-                icon: "gg-smartphone-chip",
-                value: "Do more and do it faster with the next-generation M2 or M2 Pro chip"
-            }
+                icon: "gg-card-hearts",
+                value: "Get deep insights into your physical and mental health including heart rate, Footnote 1 mood, and more"
+            },
         ],
         colors: [
             "#8C8C8C",
