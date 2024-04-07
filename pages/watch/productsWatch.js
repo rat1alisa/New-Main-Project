daa6aac952f77e1b1788310c271f1612861cdaae
Alice <alekseevaalisa763@gmail.com>
2024-04-07 19:27:38 +0300

    07/04/24

--- pages/watch/productsWatch.js
+++ pages/watch/productsWatch.js
@@ -1,6 +1,6 @@
 
 
-const products = [
+/*const products = [
     {
         id: 1,
         name: "Apple Watch Series 9",
@@ -121,5 +121,151 @@ const products = [
             "#8C8C8C",
         ],
         
+    },
+];*/
+
+
+const products = [
+    {
+        id: 1,
+        name: "Apple Watch Series 9",
+        price: "From $399",
+        images: [
+            "https://my-store.by/wp-content/uploads/2023/09/Group-130.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-s9-digitalmat-gallery-2-202309?wid=728&hei=666&fmt=png-alpha&.v=1692847849852",
+            "https://redstore.by/wp-content/uploads/2023/09/watch-9-steel-41-3.png",
+
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/s9-case-select-202309-stainless-gold?wid=5120&hei=3280&fmt=p-jpg&qlt=80&.v=d0NUMm84V3AvNk9zUmI5MXRTL1o5Nis1TkpXWGFOcTU3QmlsdXJ5TEVqMVphbVpDMUpyQUk2RzVFSHppWkNxUnc2NytHaDA2aFdROEtrekNxcXV6T3I5QTBhMSs2SW5aRXVMeUpUR3lNUFZrZ0FTWDBGNlZXTzBBbmlOa0xyRU0",
+        parametrs: [
+            {
+                icon: "gg-shape-square",
+                value: "Brighter Always-On Retina display with a durable, crack-resistant front crystal"
+            },
+            {
+                icon: "gg-card-hearts",
+                value: "Get deep insights into your physical and mental health including heart rate, Footnote 1 mood, and more"
+            },
+            {
+                icon: "gg-drive",
+                value: "The all-new S9 SiP powers a magical new way to use your watch without touching the screen"
+            },
+        ],
+        colors: [
+            "#EFD1DF",
+            "#E5E5E5",
+            "#BFBFBF",
+            "#404040",
+        ],
+        text:"Case size. Pick the one that suits you.",
+        storage:[
+            "41mm",
+            "45mm",
+        ],
+    },
+    {
+        id: 2,
+        name: "Apple Watch Ultra 2",
+        price: "From $699",
+        images: [
+            "https://inventstore.in/wp-content/uploads/2023/09/watch-ultra-double-1.webp",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-ultra2-digitalmat-gallery-1-202309?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1692603382757",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-ultra2-digitalmat-gallery-3-202309?wid=728&hei=666&fmt=png-alpha&.v=1692603384550",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-ultra2-digitalmat-gallery-5-202309?wid=728&hei=666&fmt=png-alpha&.v=1692603386501"
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/MT653_VW_34FR+watch-49-titanium-ultra2_VW_34FR+watch-face-49-ocean-ultra2_VW_34FR?wid=5120&hei=3280&bgc=fafafa&trim=1&fmt=p-jpg&qlt=80&.v=VHdUV3VJeUQyYW5oUlVUN3ZHS1VsL3Z4OU1ucmxYekw5aGF6a2ZZcG9hazdPN2hFakIxbjdEcTdvSm41NlVxWjY4QUVCdUdBMlBPcFgrL3RBcVB1cmY5akxmSjBzTThIaWphQnAvU3pXTzNTWjFsNUFraXNWRTlBaFlOTElGbFk5bW14S3ZHdXh2UXJNbDIwWGpJQ0lvV3ZaSm0rdDM0SjBUaHdZNFB5UTVYUktNUmhueHdKZy8wM2JNMm5jUVdqYm1RQnNFZEd6WG5ZMjVZbnpXOERsdz09",
+        parametrs: [
+            {
+                icon: "gg-circleci",
+                value: "The most accurate GPS in a sports watch in dense urban environments Footnote 1"
+            },
+            {
+                icon: "gg-battery-empty",
+                value: "Up to 36 hours of battery life, up to 72 hours on low power settings, Footnote 2 and up to 17 hours of workout use in Low Power Mode Footnote 3"
+            },
+            {
+                icon: "gg-drive",
+                value: "The all-new S9 SiP powers a magical new way to use your watch without touching the screen"
+            },
+        ],
+        colors: [
+            "#979797",
+        ],
+        text:"Band. Choose your adventure",
+        storage:[
+            "Alpine Loop",
+            "Ocean Band",
+            "Trail Loop",
+        ],
+    },
+    {
+        id: 3,
+        name: "Apple Watch SE",
+        price: "From $249",
+        images: [
+            "https://redstore.by/wp-content/uploads/2022/09/Apple-Watch-se-2022-40-midnight-0.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-se-digitalmat-gallery-3-202403?wid=728&hei=666&fmt=png-alpha&.v=1708659608399",
+            "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:540/bg:f6f6f6/q:100/plain/s3://catalog-products/231205074649919687/231205080743983558.png@webp"
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/se-case-select-202309-starlight?wid=5120&hei=3280&fmt=p-jpg&qlt=80&.v=Kzl5VjJlYy8yNE5uZ1M0cU0rKzBvQlZ1eTJ2b0VFMjVCSVhaLytXOG14NFI3Z0VwcktLcHpDNVRjZXc0RllqakprY3crUWRsN1dqVjRnMHR5S1hVblB2Si95b2loQkRmUVk2cVN3dmc3R0oxRmFOcWdwaDlXeGdTZDNwU2hhcUU",
+        parametrs: [
+            {
+                icon: "gg-components",
+                value: "Call and message even when you don't have your iPhone nearby with GPS + Cellular models Footnote 1"
+            },
+            {
+                icon: "gg-code",
+                value: "Innovative safety features including Emergency SOS,  footnote  2 Fall Detection, and Crash Detection"
+            },
+            {
+                icon: "",
+                value: "Get advanced workout metrics and views"
+            }
+        ],
+        colors: [
+            "#a7a6a3",
+            "#cfcfcd",
+            "#415C71",
+            "#59595c"
+        ],
+        text:"Case size. Pick the one that suits you.",
+        storage:[
+            "40mm",
+            "44mm",
+        ],
+    },
+    {
+        id: 4,
+        name: "Apple Watch Hermès",
+        price: "From $599",
+        images: [
+            "https://main-cdn.sbermegamarket.ru/big1/hlr-system/-10/646/354/381/121/11/100047510721b0.jpeg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-hermes-digitalmat-gallery-2-202403?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1708249275498",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-hermes-digitalmat-gallery-5-202403?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1708671804497",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/watch-hermes-digitalmat-gallery-3-202403?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1708249275582",
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/s9-hermes-case-select-202403-silver?wid=5120&hei=3280&fmt=p-jpg&qlt=80&.v=MFd0Mk5RTTl5U2xaTmVmUEV4R21jZmZFaVF0TWhTREJKZ20vcVEvNE9OSFRqcEF4NDhxOUV5dmhxU3hTRlhVZWZFejgveGhwTytGYVVSL250YW5HSzIyVWh6UEx5YXNDSnJiQWlSK0pJWHJ6bnlXZlFVdkZCQmROajhNK2g4Nk0",
+        parametrs: [
+            {
+                icon: "gg-asterisk",
+                value: "Exclusive Hermès bands and watch faces"
+            },
+            {
+                icon: "gg-drive",
+                value: "The all-new S9 SiP powers a magical new way to use your watch without touching the screen"
+            },
+            {
+                icon: "gg-card-hearts",
+                value: "Get deep insights into your physical and mental health including heart rate, Footnote 1 mood, and more"
+            },
+        ],
+        colors: [
+            "#8C8C8C",
+        ],
+        text:"Case size. Pick the one that suits you.",
+        storage:[
+            "41mm",
+            "45mm",
+        ],
     },
 ];
