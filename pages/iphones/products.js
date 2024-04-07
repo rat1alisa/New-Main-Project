468be5a0251b1c8705a92c6520c001bd0a127e37
Alice <alekseevaalisa763@gmail.com>
2024-04-07 19:27:05 +0300

    07/04/24

--- pages/iphones/products.js
+++ pages/iphones/products.js
@@ -1,6 +1,6 @@
 
 
-const products = [
+/*const products = [
     {
         id: 1,
         name: "iPhone 15 Pro Max",
@@ -221,7 +221,277 @@ const products = [
             "#8A0000",
         ],
     }
-];
+];/*
 
 
 /* gg-touchpad*/
+
+
+
+
+
+
+
+
+
+const products = [
+    {
+        id: 1,
+        name: "iPhone 15 Pro Max",
+        price: "From $1199",
+        images: [
+            "https://mobilworld.by/upload/iblock/9c5/9c566a95b3fd15454e27cd7a21c8ca02.png",
+            "../../images/iphones15ProMax1.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15promax-digitalmat-gallery-2-202309?wid=364&hei=333&fmt=png-alpha&.v=1693081545196",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15promax-digitalmat-gallery-4-202309?wid=364&hei=333&fmt=png-alpha&.v=1693081545196"
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1710798516323",
+        parametrs: [
+            {
+                icon: "gg-shape-square",
+                value: "Strong and light titanium design with Action button — a fast track to your favorite feature"
+            },
+            {
+                icon: "gg-arrow-long-right-r",
+                value: "USB-C connector with USB 3 for up to 20x faster transfer speeds footnote ² and new pro workflows"
+            },
+            {
+                icon: "gg-camera",
+                value: "48MP Main camera for super-high-resolution photos and 3x Telephoto camera"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "A17 Pro chip delivers a massive leap in graphics performance, transforming mobile gaming"
+            }
+        ],
+        colors: [
+            "#a7a6a3",
+            "#cfcfcd",
+            "#415C71",
+            "#59595c"
+        ],
+        text:"Storage. How much space do you need?",
+        storage:[
+            "256GB",
+            "512GB",
+            "1TB",
+        ],
+        
+    },
+    {
+        id: 2,
+        name: "iPhone 15 Pro",
+        price: "From $999",
+        images: [
+            "https://my-store.by/wp-content/uploads/2023/09/Group-80.png",
+            "../../images/iphones15ProMax1.png",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15pro-digitalmat-gallery-2-202309?wid=364&hei=333&fmt=png-alpha&.v=1693081541434",
+            "https://mobilworld.by/upload/iblock/c7e/c7efdccd3f82b1c92d84181ddede5572.png"
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1710798516323",
+        parametrs: [
+            {
+                icon: "gg-shape-square",
+                value: "Strong and light titanium design with Action button — a fast track to your favorite feature"
+            },
+            {
+                icon: "gg-arrow-long-right-r",
+                value: "USB-C connector with USB 3 for up to 20x faster transfer speeds footnote ² and new pro workflows"
+            },
+            {
+                icon: "gg-camera",
+                value: "48MP Main camera for super-high-resolution photos and 3x Telephoto camera"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "A17 Pro chip delivers a massive leap in graphics performance, transforming mobile gaming"
+            }
+        ],
+        colors: [
+            "#a7a6a3",
+            "#cfcfcd",
+            "#415C71",
+            "#59595c"
+        ],
+        text:"Storage. How much space do you need?",
+        storage:[
+            "256GB",
+            "512GB",
+            "1TB",
+        ],
+    },
+    {
+        id: 3,
+        name: "iPhone 15",
+        price: "From $899",
+        images: [
+            "https://mobilworld.by/upload/iblock/bda/bda42dff2feebdcadb7dd7ee5eac6ddd.png",
+            "https://storage.googleapis.com/stateless-creative-it-ie/2023/11/b49c3de0-finish_iphone__bvnqblbns6r6_large_2x.jpg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone15-digitalmat-gallery-2-202309?wid=364&hei=333&fmt=png-alpha&.v=1693011169058",
+            "https://mobilecare.sk/wp-content/uploads/2023/09/apple-iphone-15-plus-512-gb-modra-mu1p3sx-a-466631.jpg",
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692810665888",
+        parametrs: [
+            {
+                icon: "gg-arrows-expand-right",
+                value: "6.1-inch durable color-infused glass and aluminum design footnote ◊ with Ceramic Shield front"
+            },
+            {
+                icon: "gg-toolbar-top",
+                value: "Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else"
+            },
+            {
+                icon: "gg-camera",
+                value: "48MP Main camera with 2x Telephoto captures breathtaking detail up close or far away"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life"
+            }
+        ],
+        colors: [
+            "#5a5a5a",
+            "#DBFFFE",
+            "#DBFFDF",
+            "#FFF9DB",
+            "#FEF6FD",
+        ],
+        text:"Storage. How much space do you need?",
+        storage:[
+            "128GB",
+            "256GB",
+            "512GB",
+        ],
+    },
+    {
+        id: 4,
+        name: "iPhone 14 Plus",
+        price: "From $799",
+        images: [
+            "https://manyphone.ru/upload/iblock/aa3/zn6myzavb1av890ryplr4zhw6mao6ccm.jpg",
+            "https://focus.independent.ie/thumbor/feKL5Yaw-o5rW_P2sXP09ehaxVs=/0x24:2193x1486/960x640/prod-mh-ireland/0a806fd0-a9fb-11ed-bc1e-0210609a3fe2.jpeg",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone14plus-digitalmat-gallery-2-202209?wid=364&hei=333&fmt=png-alpha&.v=1662055813701",
+            "https://media.studio7thailand.com/93310/iPhone_14_Plus_1-square_medium.jpg",
+            
+        ],
+        orderPic: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-model-unselect-gallery-1-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660689596976",
+        parametrs: [
+            {
+                icon: "gg-shape-square",
+                value: "6.7-inch durable design footnote 1 with Ceramic Shield and water and dust resistance"
+            },
+            {
+                icon: "gg-battery-empty",
+                value: "All-day battery life with up to 26 hours of video playback²"
+            },
+            {
+                icon: "gg-camera",
+                value: "A Pro-level Main camera and advanced image processing for sensational shots in all kinds of light"
+            },
+            {
+                icon: "gg-smartphone-chip",
+                value: "The A15 Bionic processor (the same in iPhone 13 Pro)"
+            }
+        ],
+        colors: [
+            "#FFFFFF",
+            "#00021F",
+            "#B5C9E3",
+            "#F1EBFF",
+            "#FDFF7A"
+        ],
+        text:"Storage. How much space do you need?",
+        storage:[
+            "128GB",
+            "256GB",
+            "512GB",
+        ],
+        },
+        {
+            id: 5,
+            name: "iPhone 13",
+            price: "From $599",
+            images: [
+                "https://mobillife.by/images/virtuemart/product/30059037b.jpg",
+                "../../images/iphones13.png",
+                "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone13-digitalmat-gallery-2-202203?wid=364&hei=333&fmt=png-alpha&.v=1644988764167",
+                "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone13-digitalmat-gallery-4-202203?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1644988764203"
+            ],
+            orderPic: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-finish-unselect-gallery-1-202207_GEO_US?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1654894184756",
+            parametrs: [
+                {
+                    icon: "gg-shape-square",
+                    value: "Ceramic Shield, and water and dust resistance footnote²"
+                },
+                {
+                    icon: "gg-dice-5",
+                    value: "5G for superfast downloads and high-quality streaming"
+                },
+                {
+                    icon: "gg-camera",
+                    value: "Dual-camera system for incredible low-light photos and videos"
+                },
+                {
+                    icon: "gg-smartphone-chip",
+                    value: "The A15 Bionic processor with 4-core GPU"
+                }
+            ],
+            colors: [
+                "#F8E3F2",
+                "#FFFFFF",
+                "#158ABC",
+                "#001D29",
+                "#00290C"
+            ],
+            text:"Storage. How much space do you need?",
+            storage:[
+                "128GB",
+                "256GB",
+                "512GB",
+            ],
+        },
+        {
+            id: 6,
+            name: "iPhone SE",
+            price: "From $429",
+            images: [
+                "https://app-minsk.by/files/items/4440/icon_480x390.png",
+                "https://images.macrumors.com/t/iKQSY0P5NuLjNXEQBXXj7gmRowM=/1600x1200/smart/article-new/2022/03/iPhone-SE-3.jpg",
+                "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphonese-digitalmat-gallery-2-202203?wid=364&hei=333&fmt=jpeg&qlt=95&.v=1646075827246",
+                "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphonese-digitalmat-gallery-4-202203?wid=364&hei=333&fmt=png-alpha&.v=1644987579760"
+            ],
+            orderPic: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-se-finish-unselect-gallery-1-202207?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1655249921393",
+            parametrs: [
+                {
+                    icon: "gg-arrows-expand-right",
+                    value: "4.7-inch Retina HD display footnote that’s bright, colorful, and sharp"
+                },
+                {
+                    icon: "gg-live-photo",
+                    value: "Home button with Touch ID — securely unlock, sign in to apps, and use Apple Pay"
+                },
+                {
+                    icon: "gg-camera",
+                    value: "Smarter camera makes automatic adjustments so faces, places, everything looks fabulous"
+                },
+                {
+                    icon: "gg-smartphone-chip",
+                    value: "The A15 Bionic processor (the same in iPhone 13)"
+                }
+            ],
+            colors: [
+                "#FFFFFF",
+                "#121212",
+                "#8A0000",
+            ],
+            text:"Storage. How much space do you need?",
+            storage:[
+                "64GB",
+                "128GB",
+                "256GB",
+            ],
+        }
+    ];
+    
+    
+    /* gg-touchpad*/
