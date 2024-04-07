8ccce847ce0354d437e9e170c71a172677774ff5
Alice <alekseevaalisa763@gmail.com>
2024-04-07 19:26:38 +0300

    07/04/24

--- pages/ipad/ipad.js
+++ pages/ipad/ipad.js
@@ -101,6 +101,8 @@ function showProduct(productId) {
         //$('#modalName').html(nameHtml);
         $('#addColor').html(colorHtml);
 
+        $('.modal-footer').html(`<button type="button" class="btn" onclick="buyProduct(${product.id}, 'ipad/productsIPad.js')">Buy</button>`);
+
         $('#staticBackdrop').on('shown.bs.modal', function () {
             $('#modal-slider').slick();
         });
