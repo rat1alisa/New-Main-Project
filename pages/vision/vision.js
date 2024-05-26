673235ca91b2d7e90f7bfc34270ccaf231a8ecb7
Alice <alekseevaalisa763@gmail.com>
2024-05-26 16:03:21 +0300

    all!!!!

---
+++ pages/vision/vision.js
@@ -0,0 +1,37 @@
+
+
+
+$('.autoSliderVision').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+  autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
